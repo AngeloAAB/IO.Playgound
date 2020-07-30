dfdef38d6b8a6e01b8bf87942d5a47dd6c6647b1
AngeloAAB <angelo.bartholomeus@gmail.com>
2020-07-30 07:35:30 -0400

    Add project files.

---
+++ IO.Playgound/FolderManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IO.Playgound
+{
+    public class FolderManager
+    {
+        public void Create(string folderName, string dirPath = @"..\..\..\")
+        {
+            if (Directory.Exists($"{dirPath}{folderName}"))
+            {
+                Console.WriteLine($"Folder {folderName} already Exists");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{dirPath}{folderName}");
+                Console.WriteLine($"Folder {folderName} succesfully created.");
+            }            
+        }
+
+        public void Create(string[] folderNames, string dirPath = @"..\..\..\")
+        {
+            foreach (var folderName in folderNames)
+            {
+
+                if (Directory.Exists($"{dirPath}{folderName}"))
+                {
+                    Console.WriteLine($"Folder {folderName} already Exists");
+                }
+                else
+                {
+                    Directory.CreateDirectory($"{dirPath}{folderName}");
+                    Console.WriteLine($"Folder {folderName} succesfully created.");
+                }            
+            }
+        }
+
+        public void Delete(string folderName, bool deleteSubs, string dirPath = @"..\..\..\")
+        {
+            if (Directory.Exists($"{dirPath}{folderName}"))
+            {
+                Console.WriteLine($"Are you sure you want to delete {folderName}?");
+                Console.WriteLine("Enter Y for Yes/ N for No");
+                var T = Console.ReadLine();
+                bool request = T != "Y" ? false : true;
+                if (request)
+                {
+                    try
+                    {
+                        Directory.Delete($"{dirPath}{folderName}", deleteSubs);
+
+                        Console.WriteLine($"{folderName} Delete successfully");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Can't Delete {folderName}, Error {e.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IO.Playgound/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IO.Playgound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dir = new FolderManager();
+            dir.Create("Test");
+            dir.Create(@"Test\Sub Test");
+            dir.Delete("Test", true);
+
+            string[] folders =
+            {
+                @"Workspace\",
+                @"Workspace\Archive",
+                @"Workspace\Temp"
+            };
+
+            dir.Create(folders);
+
+        }
+    }
+}
+
